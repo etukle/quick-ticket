a9ac34a2980a609c0773b992f1457130eee22d99
Evren Tükle <tukleevren@gmail.com>
2025-05-22 18:11:26 +0100

    Implements prisma and sentry installations

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f70225f7c2942b00431d137af6698f0f@o4509367615881216.ingest.de.sentry.io/4509367621976144",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
