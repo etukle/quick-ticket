133c5a4c77440c8492807908122cc5cf8014c0e1
Evren Tükle <tukleevren@gmail.com>
2025-06-02 18:30:50 +0100

    Implements Tickets List Page

--- src/actions/ticket.action.ts
+++ src/actions/ticket.action.ts
@@ -54,3 +54,19 @@ export async function createTicket(
     }
 
 }
+
+export async function getTickets() {
+    try {
+        const tickets = await prisma.ticket.findMany({
+            orderBy: {createdAt: 'desc'}
+        })
+
+        logEvent("Fetched ticket list", 'ticket', {count: tickets.length}, 'info')
+        return tickets
+
+    } catch (error) {
+        logEvent('Error fetching tickets', 'ticket', {}, 'error', error)
+
+        return []
+    }
+}
