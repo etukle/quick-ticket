4711763a14ac0fc6c3dcc432fdf8f617ce633d4b
Evren Tükle <tukleevren@gmail.com>
2025-06-03 16:28:20 +0100

    Implements Ticket Details Page

--- src/actions/ticket.action.ts
+++ src/actions/ticket.action.ts
@@ -70,3 +70,21 @@ export async function getTickets() {
         return []
     }
 }
+
+export async function getTicketById(id: string) {
+    try {
+        const ticket = await prisma.ticket.findUnique({
+            where: {id: Number(id)}
+        })
+
+        if (!ticket) {
+            logEvent('Ticket not found.', 'ticket', {ticketId: id})
+        }
+
+        return ticket
+    } catch (error) {
+        logEvent('Error fetching ticket details', 'ticket', {ticketId: id}, 'error', error)
+
+        return null
+    }
+}
